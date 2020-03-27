7ab29b6ae9be86de650fa051ca1eec4d3347d00c
Mayra Peter <mayra11peter@gmail.com>
2020-03-27 11:20:57 -0300

    falta Kg e calcular os finais

--- APS.py
+++ APS.py
@@ -2,8 +2,11 @@ import numpy as np
 import math
 
 from funcoesTermosol import importa
+from funcoesTermosol import plota
 [nn,N,nm,Inc,nc,F,nr,R] = importa('entrada.xlsx')
 
+
+#plota(N, Inc)
 #numero de nos [nn]
 #matriz dos nos [N]
 #numero de membros [nm]
@@ -13,7 +16,6 @@ from funcoesTermosol import importa
 #numero de restricoes [nr]
 #vetor de restricoes [R]
 
-
 def SenCos(x1,x2,y1,y2):
     L = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
     sen = (y2 - y1)/L
@@ -21,56 +23,70 @@ def SenCos(x1,x2,y1,y2):
 
     return L,sen,cos
 
-def getCoor(N, nm):
+def getCoorNos(N, n_no):
+    nos = []
+    for i in range(0,len(N[0])):
+        nos.append([N[0][i], N[1][i]])
+    n_no = int(n_no)
+    return (nos[n_no-1])
+
+def getCoorElemento(nm, N):
     elemento = []
-    for i in range(0,nm):
-        elemento.append([N[0][i], N[1][i]])
+    for i in range(nm):
+        elemento.append([getCoorNos(N, Inc[i,0]), getCoorNos(N, Inc[i,1])])   
     return elemento
 
-def Ke(X1,X2,Y1,Y2,E,A):
-
+def Ke(coor1_elemento,coor2_elemento,E,A):
+    X1 = coor1_elemento[0]
+    Y1 = coor1_elemento[1]
+    X2 = coor2_elemento[0]
+    Y2 = coor2_elemento[1]
     l, Sen, Cos = SenCos(X1,X2,Y1,Y2)
     k = (E * A)/ l 
     #forma matriz
-    ke = [[Cos**2, Cos*Sen, -Cos**2, -Cos*Sen],[Cos*Sen, Sen*Sen, -Cos*Sen, -Sen*Sen], [-Cos**2, -Cos*Sen, Cos**2, Cos*Sen], [-Cos*Sen, -Sen*Sen, Cos*Sen, Sen*Sen]] 
-    for i in range(0, len(ke)):
-        for j in range(0,len(ke[0])):
-            ke[i][j] = ke[i][j] * k
-    return ke
+    ke = np.array([[Cos**2, Cos*Sen, -Cos**2, -Cos*Sen],[Cos*Sen, Sen*Sen, -Cos*Sen, -Sen*Sen], [-Cos**2, -Cos*Sen, Cos**2, Cos*Sen], [-Cos*Sen, -Sen*Sen, Cos*Sen, Sen*Sen]])
+
+    return (np.multiply(ke,  k))
   
 #Programa main
-for i in range(0,nm):
-    E = Inc[i,2]
-    A = Inc[i,3]
-
-coor_nos = getCoor(N, nm)
-print('cordenadas', coor_nos)
-print('nm', nm)
-
+coor_elementos = getCoorElemento(nm, N)
 #cada ke para cada elemento
 
 ke_g = []
 E = []
 A = []
-
 for i in range(0,nm):
     E.append(Inc[i,2])
     A.append(Inc[i,3])
 
+
+
 for i in range(0,nm - 1):
-    ke_g.append(Ke(coor_nos[i][0], coor_nos[i+1][0], coor_nos[i][1], coor_nos[i+1][1], E[i], A[i]))
+    ke_g.append(Ke(coor_elementos[i][0], coor_elementos[i][1], E[i], A[i]))
 
-ke_g.append(Ke(coor_nos[0][0], coor_nos[nm - 1][0], coor_nos[0][1], coor_nos[nm - 1][1], E[nm - 1], A[nm - 1]))
+ke_g.append(Ke(coor_elementos[0][0], coor_elementos[0][1], E[nm - 1], A[nm - 1]))
 
-g_liber = int(input('Digite o numero de graus de liberdade'))
 
+print(ke_g)
+g_liber = 2
 Kg = np.zeros([nm*g_liber, nm*g_liber])
 for i in range(0, Kg.shape[0]-len(ke_g)):    
     for j in range(0, len(ke_g)):
         Kg[i:i+4, i:i+4] += ke_g[j]
-    
 
-print(Kg)
+#montagem de vetor de carga global
+
+Pg = F
+
+#aplicar condicoes de contorno
+for each in R:
+    Kg = np.delete(Kg, int(each[0]), 0) 
+    Kg = np.delete(Kg, int(each[0]), 1) 
+#print(Kg)
+#solucao de sistemas de equação (solucao.py)
+
+#determinaçao das reacoes de apoio estrutural/ deformacao e tensao do elemento
+    
 
 
 
